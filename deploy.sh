6defca01ae71d21e55d569fc4ff90300d87fd336
Guillaume Flandre <guiome.flandre@gmail.com>
2014-02-02 19:22:33 +0100

    sochi data update

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ mv ./* tmp/
 cp -r tmp/html/* .
 rm -Rf tmp
 git add .
-git commit -a m"sochi data update"
+git commit -a -m"sochi data update"
 git push origin gh-pages
 git checkout master
 git branch -D gh-pages
